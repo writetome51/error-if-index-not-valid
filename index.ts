8f294cb7b2212cad18a90af55c8743b325ba6795
Steve Thompson <writetome51@gmail.com>
2019-03-30 16:48:06 -0600

    initial commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { indexIsValid } from '@writetome51/index-is-valid';
+
+
+// index can be negative or positive.
+
+export function errorIfIndexNotValid(index, arrayLength): void {
+	if (!(indexIsValid(index, arrayLength))) {
+		throw new Error(`The entered index is not valid.  Whether positive or negative,
+		it exceeds the index range of the array.`);
+	}
+}
